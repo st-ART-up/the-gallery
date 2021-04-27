3e22ef4deb74130888969c1a1496ffefe1a298d6
Erich Sauer <73718130+erichsauer@users.noreply.github.com>
2021-04-27 10:28:18 -0700

    erich branch (creates dev branch) (#1)
    * adds failing post drawing test
    
    * passing post test
    
    * adds failing get drawing by id test
    
    * adds passing get drawing by id test and failing get all drawings test
    
    * adds passing get all by artist test
    
    * adds passing get selected qty test
    
    * adds failing update drawing by use and id test
    
    * passing update drawing by user and id test
    
    * adds failing delete drawing by id test
    
    * tests passing
    
    * built inquire model. (#2)
    
    Co-authored-by: Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
    
    * fixes merge conflict
    
    * Brandon (#3)
    
    * add files from sandbox repo: controllers/auth.js, utils/auth.js, utils/imgur.js; files still have to be updated from prior dependencies
    
    * update auth controllers and utils to function using axios and to work together in a flow
    
    * update imgur for axios
    
    Co-authored-by: Brandon Perard <brandon.perard@gmail>
    
    Co-authored-by: stephen-leisy <72354883+stephen-leisy@users.noreply.github.com>
    Co-authored-by: Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
    Co-authored-by: Brandon Perard <brandon.perard@gmail.com>
    Co-authored-by: Brandon Perard <brandon.perard@gmail>

---
+++ utils.js
@@ -0,0 +1,49 @@
+const { default: axios } = require('axios');
+const displayImage = require('display-image');
+const { stARTupSkeleton } = require('.');
+
+const { anonymousGalleryPrompt } = require('./prompts');
+
+const URL = 'https://futuramaapi.herokuapp.com/api/quotes';
+
+const getAllImages = async () => {
+  const allImages = await axios.get(`${URL}/2`);
+  allImages.data.forEach((element) => {
+    displayImage
+      .fromURL(element.image)
+      .then((image) => console.log(image, 'Press arrows keys to continue'));
+  });
+};
+
+const getRandomImage = async () => {
+  const randomImage = await axios.get(`${URL}/1`);
+
+  displayImage
+    .fromURL(randomImage.data[0].image)
+    .then((image) => console.log(image, 'Press arrows keys to continue'));
+  console.log(`
+
+  ${randomImage.data[0].character}
+  ${randomImage.data[0].quote}`);
+};
+
+const getUserDrawings = async () => {
+  const allDrawings = await axios.get();
+  allDrawings.data.forEach((element) => {
+    displayImage
+      .fromURL(element.image)
+      .then((image) => console.log(image, 'Press arrows keys to continue'));
+  });
+};
+
+const deleteADrawing = async () => {
+    const deletedDrawing = await axios.delete()
+}
+
+module.exports = {
+  getAllImages,
+  getRandomImage,
+  getUserDrawings,
+};
+
+
