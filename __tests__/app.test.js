baf8d4ebef3e8bd52d3489804ce863f0df6f61b3
Erich Sauer <erich.sauer@gmail.com>
2021-04-26 17:15:06 -0700

    adds passing get selected qty test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -63,4 +63,27 @@ describe('stARTup-gallery routes', () => {
     const { body } = await request(app).get(`/api/v1/drawings`);
     expect(body).toEqual([testDrawing]);
   });
+
+  it('gets a specified number of drawings from the database', async () => {
+    const array = [1, 2, 3, 4, 5];
+    for (num of array) {
+      await Drawing.create({
+        drawingUrl: `http://placekitten.com/50${num}`,
+        title: `test kitten ${num}`,
+        caption: `test caption ${num}`,
+        username: 'test_user',
+      });
+    }
+    const { body } = await request(app).get(`/api/v1/drawings/qty/2`);
+    expect(body).toEqual([
+      testDrawing,
+      {
+        artist: 'test_user',
+        caption: 'test caption 1',
+        drawingUrl: 'http://placekitten.com/501',
+        id: '2',
+        title: 'test kitten 1',
+      },
+    ]);
+  });
 });
