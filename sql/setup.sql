1dad46e1401b86ed2f3e7a403486fe9ed0b10889
Erich Sauer <erich.sauer@gmail.com>
2021-04-27 15:54:57 -0700

    heroku stuff

--- sql/setup.sql
+++ sql/setup.sql
@@ -14,4 +14,4 @@ CREATE TABLE drawings (
     artist TEXT NOT NULL REFERENCES users(github_username)
 );
 
-INSERT INTO drawings (drawing_url, title, caption, artist) VALUES ('http://placekitten.com/500', 'test kitten drawing', 'caption of test kitten drawing', 'test_artist')
+-- INSERT INTO drawings (drawing_url, title, caption, artist) VALUES ('http://placekitten.com/500', 'test kitten drawing', 'caption of test kitten drawing', 'test_artist')
